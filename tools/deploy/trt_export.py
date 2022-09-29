bc3d61f3d4a7def79b9e2e799bcd5a04309f2995
Mikhail Scherbina <42784580+awarebayes@users.noreply.github.com>
2022-09-29 11:42:37 +0300

    Bump tensorrt version in export

--- tools/deploy/trt_export.py
+++ tools/deploy/trt_export.py
@@ -10,12 +10,11 @@ import sys
 
 import tensorrt as trt
 
+from .trt_calibrator import FeatEntropyCalibrator
+
 sys.path.append('.')
 
 from fastreid.utils.logger import setup_logger, PathManager
-from trt_calibrator import FeatEntropyCalibrator
-
-
 
 logger = setup_logger(name="trt_export")
 
@@ -142,13 +141,17 @@ def onnx2trt(
     if strict_type_constraints:
         config.set_flag(trt.BuilderFlag.STRICT_TYPES)
 
+    profile = builder.create_optimization_profile()
+    profile.set_shape("input", (1, 128, 256), (8, 128, 256), (8, 128, 256))
+    config.add_optimization_profile(profile)
+
     logger.info("Building an engine from file {}; this may take a while...".format(onnx_file_path))
-    engine = builder.build_cuda_engine(network)
+    engine = builder.build_serialized_network(network, config)
     logger.info("Create engine successfully!")
 
     logger.info("Saving TRT engine file to path {}".format(save_path))
     with open(save_path, 'wb') as f:
-        f.write(engine.serialize())
+        f.write(bytearray(engine))
     logger.info("Engine file has already saved to {}!".format(save_path))
 
 
